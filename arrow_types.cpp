587428f6eb36aa1215c7d07c31601882f322c307
Roman <romis2002@mail.ru>
2025-06-03 11:06:03 +0300

    remove unused stuff

--- arrow_types.cpp
+++
@@ -1,1 +0,0 @@
-#include "arrow_types.h"
--- skiff_schema.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <yt/cpp/mapreduce/client/skiff.h>
-
-using namespace NYT;
-
-NSkiff::TSkiffSchemaPtr SkiffSchemaFromTypeV3(NTi::TTypePtr type);
-NSkiff::TSkiffSchemaPtr SkiffSchemaFromTableSchema(const TTableSchema& tableSchema);
